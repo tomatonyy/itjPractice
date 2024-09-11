ce00ab68ccd0d60429e1dac7efee14c98c0be50a
I7E-91\i7E-86 <hyesp789@gmail.com>
2024-09-11 17:49:43 +0900

    생성자 연습

--- src/main/java/com/practicejvfirst/jvpractice6/Application.java
+++ src/main/java/com/practicejvfirst/jvpractice6/Application.java
@@ -1,4 +1,16 @@
 package src.main.java.com.practicejvfirst.jvpractice6;
 
 public class Application {
+
+    public static void main(String[] args) {
+
+        // 생성자 초기화 id, password
+
+        Member user = new Member();
+        System.out.println(user.getInformation());
+
+    }
+        // 결과
+        // id : null, pwd : null
+
 }
--- src/main/java/com/practicejvfirst/jvpractice6/Member.java
+++ src/main/java/com/practicejvfirst/jvpractice6/Member.java
@@ -1,4 +1,33 @@
 package src.main.java.com.practicejvfirst.jvpractice6;
 
 public class Member {
+
+    private String id;
+    private String pwd;
+
+    public Member() {}
+
+    public Member(String id, String pwd) {
+        this.id = id;
+        this.pwd = pwd;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getPwd() {
+        return pwd;
+    }
+
+    public String getInformation() {
+        return "id : " + this.id + ", pwd : " + this.pwd;
+    }
+
 }
