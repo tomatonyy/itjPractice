915a432b0f1f4a952d7db3c62e6326e87aed4f19
I7E-91\i7E-86 <hyesp789@gmail.com>
2024-09-11 17:48:57 +0900

    Revert "chefmacine오류 수정"
    This reverts commit 4fc1b89ef0d60103edab5cdcaa2e12b97cc56753.

--- src/main/java/com/practicejvfirst/jvpractice5/Machine.java
+++ src/main/java/com/practicejvfirst/jvpractice5/Machine.java
@@ -1,24 +1,4 @@
-package src.main.java.com.practicejvfirst.jvpractice5;
+package src.main.java.com.practicejvfirst.object;
 
-public class Machine {
-
-    private final Machine machine = new Machine();
-
-    public void powerOn() {
-        machine.powerOn();
-    }
-
-    public void toTable() {
-        machine.toTable();
-    }
-
-    public void comeBack() {
-        machine.comeBack();
-    }
-
-    public void powerOff() {
-        machine.powerOff();
-    }
-
-
+public class machine {
 }
